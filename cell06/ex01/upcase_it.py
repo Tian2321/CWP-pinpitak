665566919e1207d0c92dcfa870b5ee99cfda8bba
68070296-Parid <paridinpitak235@gmail.com>
2026-09-23 00:47:39 +0700

    add cell06

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,5 @@
+def upcase_it(text):
+    return text.upper()
+
+
+print(upcase_it("hello"))
---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,9 @@
+def add_one(number):
+    number = number + 1
+    print(number)
+
+
+value = 42
+print(value)
+add_one(value)
+print(value)
